6ad57e112be2b644a51a013d1f16c8858a66fda5
junkdood <1354784375@qq.com>
2021-06-21 19:05:33 +0800

    Partition

--- Code/lib/utils.cpp
+++ Code/lib/utils.cpp
@@ -88,80 +88,52 @@ void generatePartitions(
     const uint64 &totalCells,
     vector<uint> &binBounaries,
     const int &NCHUNKS) {
+
+        //find binBounaries of cells
+
         double DBfraction=1.0;
         unsigned int stride=1.0/DBfraction;
-
-        //histogram the data in the first dimension
         unsigned long int *bins=new unsigned long int [nCells[0]];
-
-        //find the Bin IDs with the cumulative fraction
         double *cumulativeFrac=new double [nCells[0]];
-
-        for (unsigned int i=0; i<nCells[0]; i++)
-        {
+        for (unsigned int i=0; i<nCells[0]; i++){
             cumulativeFrac[i]=0;
             bins[i]=0;
         }
 
         unsigned int binidx=0;
         unsigned int approxTotalPointsInserted=(dataPoints[0].size()*1.0)*DBfraction;
-        // printf("\nApprox points inserted: %u", approxTotalPointsInserted);
-
-        for (unsigned long int i=0; i<dataPoints[0].size(); i+=stride)
-        {
+        
+        // sum the data
+        for (unsigned long int i=0; i<dataPoints[0].size(); i+=stride){
             binidx=(dataPoints[0][i]-minVals[0])/epsilon;
             bins[binidx]++;
         }
 
         
-        
-
-        //the first cumulative fraction is the number of points in the bim
+        // calu cumulativeFrac
         cumulativeFrac[0]=bins[0];
-        //get the number
-        for (unsigned long int i=1; i<nCells[0]; i++)
-        {
+        for (unsigned long int i=1; i<nCells[0]; i++){
             cumulativeFrac[i]=bins[i]+cumulativeFrac[i-1];
-            // printf("\nbin: %lu, cumulative frac (num): %f",i,cumulativeFrac[i]);
         }
-
-        //convert to fraction
-        for (unsigned long int i=0; i<nCells[0]; i++)
-        {
+        for (unsigned long int i=0; i<nCells[0]; i++){
             cumulativeFrac[i]=cumulativeFrac[i]/(approxTotalPointsInserted*1.0);
-            // printf("\nbin: %lu, cumulative frac (fraction): %f",i,cumulativeFrac[i]);
         }
 
         //find bin boundaries
         double fractionDataPerPartition=1.0/(NCHUNKS*1.0);
         double boundary=fractionDataPerPartition;
-        // printf("\nFrac data per partition: %f", fractionDataPerPartition);
-
+        
+        // calu binBounaries of cells
         binBounaries[0]=0;
         unsigned int cntBin=1;
-        for (unsigned long int i=0; i<nCells[0]-1; i++)
-        {
-            
-            // printf("\nBoundary: %f", boundary);
-            // printf("\ni: %lu, cntbin: %u",i,cntBin);
-            if (boundary>=cumulativeFrac[i] && boundary<cumulativeFrac[i+1])
-            {
+        for (unsigned long int i=0; i<nCells[0]-1; i++){
+            if (boundary>=cumulativeFrac[i] && boundary<cumulativeFrac[i+1]){
                 binBounaries[cntBin]=i;
                 cntBin++;
                 boundary+=fractionDataPerPartition;
-            }
-
-                
+            }  
         }
-
-
         binBounaries[NCHUNKS]=nCells[0];
-
-        // for (unsigned int i=0; i<CHUNKS+1; i++)
-        // {
-        // 	printf("\nBin boundaries (cell of total num cells): %u/%u",binBounaries[i],nCells[0]);
-        // }	
-
 
         delete [] bins;
         delete [] cumulativeFrac;
@@ -183,41 +155,19 @@ void generatePartitionDatasets(
 
         const unsigned int CHUNKS = binBounaries.size()-1;
 	
-        //compute the binids that are in the shadow region
-        if (CHUNKS>1)
-        {
-            //left-most partition doesn't have a region on the left grid edge
+        //finf bin in the shadow
+        if (CHUNKS>1){
             shadowBins.push_back(binBounaries[1]-1);
             shadowBins.push_back(binBounaries[1]);
-            
-            
-
-            //middle partitions
-            for (unsigned int i=1; i<CHUNKS-1; i++)
-            {
-                //add left shadow regions
+            for (unsigned int i=1; i<CHUNKS-1; i++){
                 shadowBins.push_back(binBounaries[i]+1);
                 shadowBins.push_back(binBounaries[i]+2);
 
-                //right shadow region
                 shadowBins.push_back(binBounaries[i+1]-1);
                 shadowBins.push_back(binBounaries[i+1]);
             }
-
-
-            //right-most partition doesn't have a region on the right grid edge
             shadowBins.push_back(binBounaries[CHUNKS-1]+1);
             shadowBins.push_back(binBounaries[CHUNKS-1]+2);
-
-            // for (unsigned int i=0; i<shadowBins.size(); i++)
-            // {
-            // 	printf("\nShadow region cell id: %u",shadowBins[i] );
-            // }
-
-
-            //check to make sure that the shadow regions are disjoint
-            //i.e., no two identical bins in the shadow region
-            //if not we need to quit
 
             bool shadow_disjoint_flag=false;
             for (unsigned int i=1; i<shadowBins.size(); i++)
@@ -232,43 +182,27 @@ void generatePartitionDatasets(
             assert(shadow_disjoint_flag==false);
 
         }
-        
-        
-        //add points to shadow region
-        for (unsigned int i=0; i<dataPoints[0].size(); i++)
-        {
+
+        //add shadow points
+        for (unsigned int i=0; i<dataPoints[0].size(); i++){
             
             unsigned int binidx=(dataPoints[0][i]-minVals[0])/epsilon;
-            //add points to the shadow region if it falls in the region
-            //These are cells in two cells on each border on the left and right of
-            //each partition, except the left and right-most partitions
             auto it = std::lower_bound(shadowBins.begin(), shadowBins.end(),binidx);
-            if(!(it == shadowBins.end() || *it != binidx))
-            {
+            if(!(it == shadowBins.end() || *it != binidx)){
                 pointIDs_shadow.push_back(i);
-                for (int j=0; j<2; j++)
-                {
-                    // NDdataPointsInShadowRegion[k][i].push_back((*NDdataPoints)[k][i]);
-                    // (*NDdataPointsInShadowRegion)[k][i]=(*NDdataPoints)[k][i];
+                for (int j=0; j<2; j++){
                     dataPoints_shadow[j].push_back(dataPoints[j][i]);
-
                 }
-                
-
-                
             }
         }
         
         
 
-        //Step1: for each point, compute its partition -- can be done in parallel
+        //Step1
         std::vector<unsigned int> mapPointToPartition; 
         mapPointToPartition.resize(dataPoints[0].size());
 
         
-        
-
-        //Some threads may get lots of inner loop iterations, need to address load imbalance
         #pragma omp parallel for num_threads(NUM_THREADS) schedule(dynamic, 1)
         for (unsigned int i=0; i<dataPoints[0].size(); i++)
         {
@@ -286,8 +220,7 @@ void generatePartitionDatasets(
         
 
         //step2: 
-        //Num threads is either the chunks or the num threads, whichever is lower
-        const unsigned int NTHREADSPARTITION=min(NUM_THREADS,CHUNKS);
+        // const unsigned int NTHREADSPARTITION=min(NUM_THREADS,CHUNKS);
         std::vector<unsigned int> prefixSumPartition[CHUNKS];
         std::vector<unsigned int> pointIDsPartition[CHUNKS];
         unsigned int prefixSums[CHUNKS]; 
@@ -297,7 +230,7 @@ void generatePartitionDatasets(
             prefixSums[i]=0;
         }
 
-        //prefix sum for each CHUNK
+        //prefix sum
         for (unsigned int i=0; i<dataPoints[0].size(); i++)
         {
             unsigned int partition=mapPointToPartition[i];
@@ -310,7 +243,7 @@ void generatePartitionDatasets(
 
         
 
-        //resize arrays
+        //resize
         for (unsigned int i=0; i<CHUNKS; i++){
             for (int j=0; j<2; j++){
             partDataPointsArray[i][j].resize(pointIDsPartition[i].size());
@@ -321,8 +254,7 @@ void generatePartitionDatasets(
         
 
         
-        //write the results using prefix sums in parallel
-
+        //make final partition
         for (unsigned int i=0; i<CHUNKS; i++)
         {
             #pragma omp parallel for num_threads(NUM_THREADS) shared(i)
@@ -336,7 +268,7 @@ void generatePartitionDatasets(
                 partDataPointsArray[i][j][idx]=dataPoints[j][dataidx];	
                 }
 
-                //update mapping for the point in the entire (global) dataset
+                
                 PointChunkLookup tmp;
                 tmp.pointID=dataidx;
                 tmp.chunkID=i;
